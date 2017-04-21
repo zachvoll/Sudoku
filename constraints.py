256b302fd31eab1b33132d78a9860ab3bb8c1060
zachvoll <rippsta@gmail.com>
2017-04-21 00:22:30 -0500

    Update constraints.py

--- constraints.py
+++ constraints.py
@@ -3,7 +3,8 @@ from itertools import product
 
 '''
 'a list->string->ConstraintVar
-
+A constraint variable with a domain (possible legal values), name (index identifier), 
+and neighbors (those cells that share a row, column, or box)
 '''
 class ConstraintVar:
     def __init__(self, d, n ):
@@ -18,7 +19,7 @@ class ConstraintVar:
                 self.neighbors.append(bc)
 '''
 ConstraintVar->('a -> bool)->UnaryConstraint
-
+Unary Constraint is used to constrain initialized sudoku cells to reduce potential values.
 '''
 class UnaryConstraint:
     def __init__(self, v, fn):
@@ -27,7 +28,8 @@ class UnaryConstraint:
 
 '''
 ConstraintVar->ConstraintVar->('a->'a->bool)->BinaryConstraint
-
+Binary Constraints are relationships between neighbors based on a function.
+In this case it's that neighbors can't be equal to each other.
 '''
 class BinaryConstraint:
     def __init__(self, v1, v2, fn):
@@ -36,7 +38,7 @@ class BinaryConstraint:
         self.func = fn
 
 '''
-
+Used to reduce the initial domains of cells due to preassigned cells.
 '''
 def nodeConsistent(uc):
     domain = list(uc.var.domain)
